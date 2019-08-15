45a276cd5b2e7359fb1299db74537ad48433b121
Mustafa Ali <alimustafa0987@gmail.com>
2019-08-14 22:05:09 -0400

    basic skeleton created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
-function App() {
-  return (
-    <div>
-      <Row noGutters="true">
-        <Col lg="3">Add Button Here</Col>
-        <Col lg="9">Text And Remaining Here</Col>
-      </Row>
-      <Row noGutters="true">
-        Color Bar Here
-      </Row>
-      <Row noGutters="true">Main Info Here</Row>
-    </div>
-  );
+class App extends Component{
+
+  constructor(){
+    super()
+    this.state={
+      bills: []
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <Row noGutters="true">
+          <Col lg="3">Add Button Here</Col>
+          <Col lg="9">Text And Remaining Here</Col>
+        </Row>
+        <Row noGutters="true">
+          Color Bar Here
+        </Row>
+        <Row noGutters="true">Main Info Here</Row>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
