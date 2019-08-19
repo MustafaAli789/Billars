6c9e25a527065af093d8e5919c4b26adb922ff82
Mustafa Ali <alimustafa0987@gmail.com>
2019-08-18 21:07:15 -0400

    Updates

---
+++ precache-manifest.4b076bfdb43216271501756de5157592.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888bf1e0e91ecf4b8443f8720bddaf52",
+    "url": "/Billars/index.html"
+  },
+  {
+    "revision": "2501248c8842c2a14459",
+    "url": "/Billars/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "cb89324cdec5b96241af",
+    "url": "/Billars/static/css/main.61a34b47.chunk.css"
+  },
+  {
+    "revision": "2501248c8842c2a14459",
+    "url": "/Billars/static/js/2.dd8c3b4d.chunk.js"
+  },
+  {
+    "revision": "cb89324cdec5b96241af",
+    "url": "/Billars/static/js/main.8be6404e.chunk.js"
+  },
+  {
+    "revision": "f15a187c8ac3211d57ea",
+    "url": "/Billars/static/js/runtime~main.a6b65a92.js"
+  }
+]);
